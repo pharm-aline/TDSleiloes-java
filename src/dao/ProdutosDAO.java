bac88baafae0177ce02faf8fd4c0f67d2e501298
Aline Cruz <acruz@unicamp.br>
2024-10-15 09:39:47 -0300

    Organização de classes em pacotes

--- src/dao/ProdutosDAO.java
+++ src/dao/ProdutosDAO.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+package dao;
 
-/**
- *
- * @author Adm
- */
 
+import dto.ProdutosDTO;
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
--- src/dto/ProdutosDTO.java
+++ src/dto/ProdutosDTO.java
@@ -1,12 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+package dto;
 
-/**
- *
- * @author Adm
- */
+
 public class ProdutosDTO {
     private Integer id;
     private String nome;
