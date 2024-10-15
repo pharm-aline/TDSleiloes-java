7b8a27b9f99c97ca12111880a6a95d06a4472275
Aline Cruz <acruz@unicamp.br>
2024-10-15 11:07:27 -0300

    Adição de exemplo para testar o cadastro

--- src/Principal.java
+++ src/Principal.java
@@ -1,10 +1,20 @@
 
+import dao.ProdutosDAO;
+import dto.ProdutosDTO;
 import view.cadastroVIEW;
 
 
 public class Principal {
     public static void main(String args[]) {
-    cadastroVIEW tela = new cadastroVIEW();
-    tela.setVisible(true);
+    //cadastroVIEW tela = new cadastroVIEW();
+    //tela.setVisible(true);
+    
+    ProdutosDTO p = new ProdutosDTO();
+    p.setNome("Teste 1");
+    p.setValor(230.5);
+    p.setStatus("Vendido");
+    
+    ProdutosDAO dao = new ProdutosDAO();
+    dao.cadastrarProduto(p);
     }
 }
